139ab7243b43e8a058697cb795dddfa0f13e1fff
Emily Sun <ESun0703@Emilys-MacBook-Pro.local>
2017-02-17 20:25:45 -0500

    added BasicCard.prototype.readCard, var newCard, newCard.readCard

--- basic.js
+++ basic.js
@@ -1,9 +1,51 @@
 "use strict";
-const processJs = require("process.js");
-function basicCard (front, back) {
-	if(!(this instanceof basicCard)) {
-		return new basicCard (front, back)
+const inquire = require("inquirer")
+const processJS = require("./process.js");
+// var question = processJS.
+function BasicCard (front, back) {//added capitalization in "basic"
+	if(!(this instanceof BasicCard)) {
+		return new BasicCard (front, back)
 	}
 	this.front=front;
 	this.back=back;
+	// console.log (this);
 }
+
+//Need Promises
+// BasicCard.prototype.readCard = function (){ 
+// 	inquire.prompt([
+// 		{
+// 		  type: "input",
+// 		  message: this.front,
+// 		  name: "answerQuestion"
+// 		},
+// 	]).then(function(card){
+// 		console.log(JSON.stringify(user, null, 2));
+// 		if(card.answerQuestion===this.back){
+// 			console.log("correct! The Answer Is Indeed " + this.back);
+// 		} else {
+// 			console.log("sorry! The Answer Is " + this.back)
+// 		};
+// 	});
+// };
+// newCard.readCard();
+
+BasicCard.prototype.readCard= function (){
+	console.log(this.front);
+	console.log(this.back);
+}
+
+// BasicCard.prototype.readBack = function(){
+// 	setTimeout(function(){
+// 		console.log(this.back);
+// 	},2000)
+// }
+
+
+var newCard = new BasicCard ("Who was the first president of the united states?", "George Washington")
+
+newCard.readCard();
+// newCard.readBack()
+
+
+
--- process.js
+++ process.js
@@ -1,12 +1,22 @@
 //Install Node
 //Install Inquire
 //Require Inquire
-//Require Cloze.js
+	//var inquire = require ("inquirer")
+//Require Cloze.js 
+	//var inquire = require ("cloze.js")
 //Require Basic.js
+	//var inquire = require ("cloze.js")
 // Two Choices: Add or Review
+	//Would You Like To Add or Review? 
 //If Add: Ask Basic or Cloze? (inquire)//write file
+	//What type of flashcard would you like to add? Basic Close?
+		//Front? Input
+		//Back? Input
 	//Basic
+		//constructor: argument 1: front, argument 2: back (done)
 	//Close
+		//constructor: argument 1: front, argument 2: back
 //If Review: Ask Basic or Cloze? (inquire)//Read File
 	//Basic?
 	//Close
+
