cd74d1c236aa92e392868abb793c3ebc1965f8cd
Emily Sun <ESun0703@Emilys-MacBook-Pro.local>
2017-02-17 19:05:02 -0500

    added pseudocode

--- process.js
+++ process.js
@@ -1,1 +1,12 @@
-Two Choices: Add or Review
+//Install Node
+//Install Inquire
+//Require Inquire
+//Require Cloze.js
+//Require Basic.js
+// Two Choices: Add or Review
+//If Add: Ask Basic or Cloze? (inquire)//write file
+	//Basic
+	//Close
+//If Review: Ask Basic or Cloze? (inquire)//Read File
+	//Basic?
+	//Close
